9641201a7d03528dc62d55669ec624cf199cf915
Anand Joseph <anajoseph@nvidia.com>
2024-12-06 04:55:22 +0530

    Initial commit
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# telephony
-Telephony related stuff
+# Telephony related stuff
+This repo primarily contains various scripts for testing out telephony.
+The scripts depend on pjsip's pjsua2 module for Python Refer: https://docs.pjsip.org/en/latest/pjsua2/building.html
+
---
+++ make_call_and_play.py
@@ -0,0 +1,106 @@
+import pjsua2 as pj
+
+import time
+import logging
+
+# Subclass to extend the Account and get notifications etc.
+class Account(pj.Account):
+    def onRegState(self, prm):
+        print("***OnRegState: " + prm.reason)
+
+    def onIncomingCall(self, prm):
+        self.call_id == prm.callId
+        self.acceptCall = True
+
+        self.c = Call(self, call_id=prm.callId)
+
+        ci = self.c.getInfo()
+        msg = "Incoming call  from  '%s'" % (ci.remoteUri)
+        print(msg)
+
+        self.inCall = True
+
+
+class Call(pj.Call):
+    """
+    High level Python Call object, derived from pjsua2's Call object.
+    """
+    def __init__(self, acc, call_id=pj.PJSUA_INVALID_ID):
+        pj.Call.__init__(self, acc, call_id)
+        self.player = None
+
+    def onCallState(self, prm):
+
+        ci = self.getInfo()
+
+
+
+        if(ci.state==pj.PJSIP_INV_STATE_DISCONNECTED):
+            print(">>>>>>>>>>>>>>>>>>>>>>> Call disconnected")
+
+        raise Exception('onCallState done!')
+
+    def onCallMediaState(self, prm):
+        print("Call media state changed")
+        ci = self.getInfo()
+        if ci.state == pj.PJSIP_INV_STATE_CONFIRMED:
+            print("Call is confirmed, starting audio playback")
+            self.player = pj.AudioMediaPlayer()  # Use the class level attribute
+            self.player.createPlayer("audio/steve.wav", pj.PJMEDIA_FILE_NO_LOOP)
+            aud_med = self.getAudioMedia(-1)
+            self.player.startTransmit(aud_med)
+        raise Exception('onCallMediaState done!')
+
+def make_call_and_play():
+    # Create and initialize the library
+    ep_cfg = pj.EpConfig()
+    ep_cfg.medConfig.channelCount = 2
+    ep = pj.Endpoint()
+    ep.libCreate()
+    ep.libInit(ep_cfg)
+
+    # Create SIP transport. Error handling sample is shown
+    sipTpConfig = pj.TransportConfig()
+    sipTpConfig.port = 5060
+    ep.transportCreate(pj.PJSIP_TRANSPORT_UDP, sipTpConfig)
+    # Start the library
+    ep.libStart()
+    pj.Endpoint.instance().audDevManager().setNullDev()
+
+    acfg = pj.AccountConfig()
+
+    acfg.idUri = "sip:5001@192.168.7.60"
+    acfg.regConfig.registrarUri = "sip:192.168.7.60"
+    cred = pj.AuthCredInfo("digest", "*", "5001", 0, "5001500150015001")
+    acfg.sipConfig.authCreds.append(cred)
+
+
+    # Create the account
+    acc = Account()
+
+    acc.create(acfg)
+    time.sleep(4)
+
+    call = Call(acc)
+    call_prm = pj.CallOpParam(True)
+
+
+    try:
+        call.makeCall('sip:1402@192.168.7.60', call_prm)
+    except Exception as e:
+        print(e)
+    while True:
+        if call.isActive():
+            time.sleep(2)
+        else:
+            break
+
+    ep.libDestroy()
+    del ep
+
+
+#
+# main()
+#
+if __name__ == "__main__":
+    make_call_and_play()
